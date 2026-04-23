dcb42ff6f0f4d450e6b711c87fde79f4cd4603d7
Fabian Quintana <quintanafabiangustavo@gmail.com>
2026-04-23 10:19:28 -0300

    feat: Ultimos arreglos para consistencia.  entraga de funcionalidad

--- InmoGestor.API/Controllers/ContratoController.cs
+++ InmoGestor.API/Controllers/ContratoController.cs
@@ -159,14 +159,14 @@ namespace InmoGestor.API.Controllers
 
         [HttpPut("rescindir/{id}")]
         [Authorize(Roles = "Superior")]
-        public IActionResult Rescindir(string id)
+        public IActionResult Rescindir(string idContrato)
         {
-            if (!Guid.TryParse(id, out var guidId))
+            if (!Guid.TryParse(idContrato, out var contratoId))
             {
                 return BadRequest(new { success = false, mensaje = "ID inválido" });
             }
 
-            var (success, message) = _cnContrato.Rescindir(guidId);
+            var (success, message) = _cnContrato.Rescindir(contratoId);
 
             if (!success)
             {
